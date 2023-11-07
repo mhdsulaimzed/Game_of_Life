892697ae4aac54d4212a41b94fe22473e5992fcd
sulaim <sulaimsaed@gmail.com>
2023-11-07 15:15:57 +0530

    Red : Implemented a test for get the cells alive in the list

--- gol.py
+++ gol.py
@@ -2,3 +2,4 @@
 def get_grid(row,cols):
    return [[0 for _ in range(cols)] for _ in range(row)]
 
+print(get_grid(3,3))
--- test_gol.py
+++ test_gol.py
@@ -4,3 +4,8 @@ from gol  import get_grid
 def test_empty_grid():
   assert get_grid(3,3) == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
  
+def test_one_cell_alive():
+  
+  grid=[[0, 0, 1], [0, 1, 0], [0, 0, 0]]
+  x,y=1,1
+  assert get_neighbour_count(grid,x,y) == 1
