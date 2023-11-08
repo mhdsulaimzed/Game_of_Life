7ef616756ab7fb191dd0b6a1fa8d525fef4251b1
sulaim <sulaimsaed@gmail.com>
2023-11-08 18:57:34 +0530

    Added  main function for  implementing logics

--- test_gol.py
+++ test_gol.py
@@ -1,73 +1,41 @@
-from gol import get_grid, get_alive_neighbor_count, upgrade_grid
+from gol  import get_grid,get_alive_neighbor_count,upgrade_grid
 
 
 def test_empty_grid():
-    assert get_grid(3, 3) == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
+  assert get_grid(3,3) == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+ 
 
 def test_one_cell_alive():
-    grid = [[0, 0, 1], [0, 1, 0], [0, 0, 0]]
-    x, y = 1, 1
-    assert get_alive_neighbor_count(grid, x, y) == 1
+  rid=[[0, 0, 1], [0, 1, 0], [0, 0, 0]]
+  x,y=1,1
+  assert get_alive_neighbor_count(grid,x,y) == 1  
 
 
 def test_multiple_alive_cell():
-    grid = [
-        [0, 0, 1],
-        [
-            1,
-            1,
-            0,
-        ],
-        [1, 0, 0],
-    ]
-    x, y = 1, 1
-    assert get_alive_neighbor_count(grid, x, y) == 3
+      grid=[[0, 0, 1 ], [1, 1, 0,], [1, 0, 0]]
+      x,y = 1, 1
+      assert get_alive_neighbor_count(grid,x,y) == 3
 
 
 def test_rule_case1():
-    grid = [
-        [1, 1, 1],
-        [
-            1,
-            1,
-            0,
-        ],
-        [0, 0, 0],
-    ]
-    x, y = 1, 1
-    assert upgrade_grid(grid) == [[1, 0, 1], [1, 0, 1], [0, 0, 0]]
+      grid=[[1, 1, 1 ], [1, 1, 0,], [0, 0, 0]]
+      x, y = 1, 1
+      assert upgrade_grid(grid) == [[1, 0, 1], [1, 0, 1], [0, 0, 0]]
 
 
 def test_rule_case2():
-    grid = [
-        [1, 0, 1],
-        [1, 0, 1],
-        [
-            0,
-            0,
-            0,
-        ],
-    ]
-    assert upgrade_grid(grid) == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    grid=[[1, 0, 1 ],[1, 0, 1],[0, 0, 0,]]
+    assert upgrade_grid(grid) == [[0, 0, 0], [0, 0, 0], [0, 0, 0]] 
 
 
 def test_rule_case3():
-    grid = [
-        [1, 1, 0],
-        [
-            1,
-            0,
-            0,
-        ],
-        [0, 0, 0],
-    ]
-    assert upgrade_grid(grid) == [
-        [1, 1, 0],
-        [
-            1,
-            1,
-            0,
-        ],
-        [0, 0, 0],
-    ]
+    grid = [[1, 1, 0 ], [1, 0, 0,], [0, 0, 0]]
+    assert upgrade_grid(grid) == [[1, 1, 0 ], [1, 1, 0,], [0, 0, 0]]
+
+
+
+
+     
+
+
+
