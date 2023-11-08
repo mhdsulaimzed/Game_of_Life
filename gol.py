6b21130481f800c9e66d021a1d5e222544bb508b
sulaim <sulaimsaed@gmail.com>
2023-11-08 17:58:00 +0530

    Implement display grid function for printing the grid in a text-based format

--- gol.py
+++ gol.py
@@ -1,89 +1,502 @@
 import time
 
 
-def get_grid(row,cols):
-   return [[0 for _ in range(cols)] for _ in range(row)]
+def get_grid(row, cols):
+    return [[0 for _ in range(cols)] for _ in range(row)]
 
 
+def get_alive_neighbor_count(grid, x, y):
+    count = 0
 
-def get_alive_neighbor_count(grid,x,y):
-   count = 0
-  
-   for i in range(-1,2):
-      for j in  range(-1,2):
-            
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             if i == 0 and j == 0:
-               continue
-            
-            if 0 <= x+i < len(grid) and 0 <= y+j < len(grid[0]):
-                  if grid[x+i][y+j] ==  1:
-                     count +=1
+                continue
 
-   return count
+            if 0 <= x + i < len(grid) and 0 <= y + j < len(grid[0]):
+                if grid[x + i][y + j] == 1:
+                    count += 1
+
+    return count
+
 
 def upgrade_grid(grid):
-    
-   updated_grid = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    updated_grid = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
 
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            neighbor_count = get_alive_neighbor_count(grid, x, y)
 
-   for x in range (len(grid)):
-      for y in range(len(grid[0])):
-
-         neighbor_count = get_alive_neighbor_count(grid, x, y)   
-            
-         if grid[x][y] == 1:
-    # Cell is currently alive
-            if 2 == neighbor_count or neighbor_count == 3:
-                  updated_grid[x][y] = 1  # Cell remains alive
+            if grid[x][y] == 1:
+                # Cell is currently alive
+                if 2 == neighbor_count or neighbor_count == 3:
+                    updated_grid[x][y] = 1  # Cell remains alive
+                else:
+                    updated_grid[x][y] = 0  # Cell dies
             else:
-                  updated_grid[x][y] = 0  # Cell dies
-         else:
-    # Cell is currently dead
-            if neighbor_count == 3:
-               updated_grid[x][y] = 1  # Cell becomes alive
-            else:
-               updated_grid[x][y] = 0  # Cell remains dead
-   return updated_grid
-            
+                # Cell is currently dead
+                if neighbor_count == 3:
+                    updated_grid[x][y] = 1  # Cell becomes alive
+                else:
+                    updated_grid[x][y] = 0  # Cell remains dead
+    return updated_grid
 
 
+def display_grid(grid):
+    """Print the grid in a text-based format."""
+    for row in grid:
+        print("".join(["\033[1;31;40m ■ " if cell == 1 else " " for cell in row]))
 
 
+def main():
+    grid = [
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+    ]
+
+    for _ in range(50):
+        print("\033c")
+        display_grid(grid)
+        time.sleep(0.2)
+        grid = upgrade_grid(grid)
 
 
-
-
-
-    
-    
-    
-    
-   
-
-   
-   
-
-
-
-
-   
-
-    
-   
-
-            
-
-
-
-
-
-
-
-    
-    
-
-
-   
-                
-   
-
+main()
